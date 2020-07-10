3eb33abb1c0d834ecd601bf650c99bb58eab7eb5
ounihamza100 <hamza.ellouni@gmail.com>
2020-07-10 14:05:35 +0100

    first commit

---
+++ src/main/java/io/houni/multitenancyspringboothibernate/MultiTenancySpringbootHibernateApplication.java
@@ -0,0 +1,13 @@
+package io.houni.multitenancyspringboothibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenancySpringbootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenancySpringbootHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/houni/multitenancyspringboothibernate/MultiTenancySpringbootHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package io.houni.multitenancyspringboothibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenancySpringbootHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
